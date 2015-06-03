ca2314848f7255153c6b873cd5baa45849acc3e0
Juliet Brown <apprentice@StMac4.local>
2015-06-03 16:55:03 +1200

    added initialize method

---
+++ google-map-javascript-API/js/map.js
@@ -0,0 +1,10 @@
+$document.ready(function() {
+  function initialize() {
+        var mapProp = {
+          center:new google.maps.LatLng(51.508742,-0.120850),
+          zoom: 7,
+          mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+  }
+});
+
